2a253e8918305ebe217516cc4ad3b1e10449e4e1
kalpana <kalpanaironbanda@gmail.com>
2023-06-10 12:31:19 +0530

    updated

--- dockerfile
+++ dockerfile
@@ -1,11 +1,14 @@
+# FROM reactjs 
+# WORKDIR /build 
+# COPY . /build 
+# CMD ['npm start']
+
 FROM node:alpine AS build_stage
 WORKDIR '/app'
-RUN rm -rf /app/*
 COPY package.json .
 RUN npm install
-                   
 COPY . .
 RUN npm run build
 
 FROM nginx
-COPY --from=build_stage /app/build /usr/share/nginx/html    
+COPY --from=build_stage /app/build /usr/share/nginx/html
--- src/App.js
+++ src/App.js
@@ -1,21 +1,145 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
-  const [studentTable, setStudentTable] = useState('');
+const StudentTable = () => {
+  const [students, setStudents] = useState([]);
+  const [employees, setEmployees] = useState([]);
+  const [workers, setWorkers] = useState([]);
+  const [salaries, setSalaries] = useState([]);
 
-  const handleClick = async () => {
-    const response = await fetch('URL_OF_YOUR_BACKEND_API');
-    const result = await response.text();
-    setStudentTable(result);
+  useEffect(() => {
+    axios.get("backend url/studentlist")
+      .then(response => {
+        setStudents(response.data.studentlist);
+      })
+      .catch(error => {
+        console.error('Error fetching student data:', error);
+      });
+
+    axios.get("backend url/employeelist")
+      .then(response => {
+        setEmployees(response.data.employeelist);
+      })
+      .catch(error => {
+        console.error('Error fetching employee data:', error);
+      });
+
+    axios.get("backend url/workerlist")
+      .then(response => {
+        setWorkers(response.data.workerlist);
+      })
+      .catch(error => {
+        console.error('Error fetching workers data:', error);
+      });
+
+    axios.get("backend url/salarylist")
+      .then(response => {
+        setSalaries(response.data.salarylist);
+      })
+      .catch(error => {
+        console.error('Error fetching salaries data:', error);
+      });
+  }, []);
+
+  const tableHeaderStyle = {
+    backgroundColor: '#333',
+    color: '#fff',
+    padding: '10px',
+    textAlign: 'left',
+  };
+
+  const tableCellStyle = {
+    padding: '10px',
+    borderBottom: '1px solid #ccc',
   };
 
   return (
-    <>
-      <h1>Welcome to the Student List</h1>
-      <button onClick={handleClick}>Get Student List</button>
-      <div dangerouslySetInnerHTML={{ __html: studentTable }} />
-    </>
-  );
-}
+    <div style={{ backgroundColor: '#f2f2f2', padding: '20px' }}>
+      <h1 style={{ color: '#333', marginBottom: '20px' }}>Student List</h1>
+      {/* Student list table */}
+      <table style={{ width: '100%', borderCollapse: 'collapse' }}>
+        <thead>
+          <tr>
+            <th style={tableHeaderStyle}>Name</th>
+            <th style={tableHeaderStyle}>Roll</th>
+            <th style={tableHeaderStyle}>Grade</th>
+          </tr>
+        </thead>
+        <tbody>
+          {students.map(student => (
+            <tr key={student.id}>
+              <td style={tableCellStyle}>{student.name}</td>
+              <td style={tableCellStyle}>{student.roll}</td>
+              <td style={tableCellStyle}>{student.grade}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
 
-export default App;
+      <h1 style={{ color: '#333', marginTop: '40px', marginBottom: '20px' }}>Employee List</h1>
+      {/* Employee list table */}
+      <table style={{ width: '100%', borderCollapse: 'collapse' }}>
+        <thead>
+          <tr>
+            <th style={tableHeaderStyle}>Name</th>
+            <th style={tableHeaderStyle}>Employee_id</th>
+            <th style={tableHeaderStyle}>Department</th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees.map(employee => (
+            <tr key={employee.id}>
+              <td style={tableCellStyle}>{employee.name}</td>
+              <td style={tableCellStyle}>{employee.employee_id}</td>
+              <td style={tableCellStyle}>{employee.department}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <h1 style={{ color: '#333', marginBottom: '20px' }}>Worker List</h1>
+      {/* Worker list table */}
+      <table style={{ width: '100%', borderCollapse: 'collapse' }}>
+        <thead>
+          <tr>
+            <th style={tableHeaderStyle}>Name</th>
+            <th style={tableHeaderStyle}>Worker_id</th>
+            <th style={tableHeaderStyle}>Job_title</th>
+          </tr>
+        </thead>
+        <tbody>
+          {workers.map(worker => (
+            <tr key={worker.id}>
+              <td style={tableCellStyle}>{worker.name}</td>
+              <td style={tableCellStyle}>{worker.worker_id}</td>
+              <td style={tableCellStyle}>{worker.job_title}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <h1 style={{ color: '#333', marginTop: '40px', marginBottom: '20px' }}>Salary List</h1>
+      {/* Salary list table */}
+      <table style={{ width: '100%', borderCollapse: 'collapse' }}>
+        <thead>
+          <tr>
+            <th style={tableHeaderStyle}>Name</th>
+            <th style={tableHeaderStyle}>Roll_id</th>
+            <th style={tableHeaderStyle}>Salary</th>
+          </tr>
+        </thead>
+        <tbody>
+          {salaries.map(salary => (
+            <tr key={salary.id}>
+              <td style={tableCellStyle}>{salary.name}</td>
+              <td style={tableCellStyle}>{salary.roll_id}</td>
+              <td style={tableCellStyle}>{salary.salary}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default StudentTable;
